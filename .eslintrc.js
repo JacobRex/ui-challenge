d5aac40d2d3fb4537823b148013f245ecd3c06ec
Jacob Rex <jacobrex@gmail.com>
2020-05-07 09:50:52 -0500

    [Architecture] Adding eslint (#1)
    * Installing and configuring eslint
    
    * Running lintfix
    
    * Adding an nvmrc
    
    * Removing unecessary code

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
+    'eslint:recommended',
+    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+    'plugin:vue/recommended'
+  ],
+  // required to lint *.vue files
+  plugins: [
+    'vue',
+  ],
+  // add your custom rules here
+  rules: {
+    'semi': [2, 'always'],
+    'no-console': 'off',
+    'quotes': ['error', 'single'],
+    'vue/max-attributes-per-line': [2, {
+      'singleline': 1,
+      'multiline': 1,
+    }],
+  }
+}
