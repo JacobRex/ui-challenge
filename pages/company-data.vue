2441d8cf50a9140d20adedbcd3b05eea35cc23a3
Jacob Rex <jacobrex@gmail.com>
2020-05-07 13:52:34 -0500

    Removing accidental commit

--- pages/company-data.vue
+++ pages/company-data.vue
@@ -1,55 +1,11 @@
 <template>
   <div>
-    <ui-form-field
-      label="Form Field"
-      :error="inputOne.error"
-    >
-      <ui-textarea
-        v-model="inputOne.value"
-        placeholder="Placeholder"
-        resizeable
-      />
-    </ui-form-field>
-
-    <ui-form-field
-      label="Form Field with Error"
-      :error="inputTwo.error"
-    >
-      <ui-textarea
-        v-model="inputTwo.value"
-        placeholder="Placeholder"
-      />
-    </ui-form-field>
+    Company Data
   </div>
 </template>
 
 <script>
-import { UiFormField } from 'Components/FormField';
-import { UiTextarea } from 'Components/Textarea';
-
 export default {
-  components: {
-    UiFormField,
-    UiTextarea,
-  },
-
-  data() {
-    return {
-      inputOne: {
-        error: null,
-        value: '',
-      },
-      inputTwo: {
-        error: 'Ruh roh Raggy!',
-        value: '',
-      },
-      inputThree: {
-        error: null,
-        value: 'Value',
-      },
-    };
-  },
-
   mounted() {
     this.$store.commit(
       'layout/setBreadCrumbs',
@@ -63,13 +19,3 @@ export default {
   },
 };
 </script>
-
-<style module="$s">
-.Field {
-  width: 300px;
-  line-height: 40px;
-  text-align: center;
-  color: var(--color-gray-300);
-  border: 1px dashed var(--color-gray-300);
-}
-</style>
