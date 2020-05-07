52f5b79af6fb105967c01fc277d639d247485ae6
Jacob Rex <jacobrex@gmail.com>
2020-05-07 12:51:59 -0500

    LoadingIndicator component

---
+++ components/LoadingIndicator/src/LoadingIndicator.vue
@@ -0,0 +1,31 @@
+<template>
+  <div :class="$s.LoadingIndicator" />
+</template>
+
+<script>
+export default {
+  name: 'UiLoadingIndicator',
+};
+</script>
+
+<style module="$s">
+@import 'Vars';
+
+.LoadingIndicator {
+  width: calc(var(--space-md) - 4px);
+  height: calc(var(--space-md) - 4px);
+  border: 2px solid currentColor;
+  border-top: 2px solid transparent;
+  border-radius: var(--space-md);
+  animation: spin 1s infinite linear;
+}
+
+@keyframes spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
