5aad0df93a0663b72ee31d30ea8debce7e2a71d8
Jacob Rex <jacobrex@gmail.com>
2020-05-07 11:46:27 -0500

    [Component] Navbar. [Feature] Adds Navbar to layout. (#8)
    * Navbar component. Logo asset
    
    * Adding the navbar to the layout
    
    * Removing unecessary template

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,14 @@
 <template>
   <ui-layout>
     <ui-layout-header sticky>
-      Header
+      <ui-navbar :navigation="navbar">
+        <ui-avatar
+          slot="actions"
+          show-arrow
+          :src="group"
+          :srcset="`${group} 1x, ${group2x} 2x, ${group3x} 3x`"
+        />
+      </ui-navbar>
     </ui-layout-header>
 
     <ui-layout-main>
@@ -21,16 +28,64 @@
 </template>
 
 <script>
+import { UiAvatar } from 'Components/Avatar';
 import { UiLayout, UiLayoutHeader, UiLayoutMain, UiLayoutSider, UiLayoutContent, UiLayoutFooter } from 'Components/Layout';
+import { UiNavbar } from 'Components/Navbar';
+
+import group from 'Static/images/group.png';
+import group2x from 'Static/images/group@2x.png';
+import group3x from 'Static/images/group@3x.png';
 
 export default {
   components: {
+    UiAvatar,
     UiLayout,
     UiLayoutHeader,
     UiLayoutMain,
     UiLayoutSider,
     UiLayoutContent,
     UiLayoutFooter,
+    UiNavbar,
+  },
+
+  data() {
+    return {
+      group,
+      group2x,
+      group3x,
+      navbar: [
+        {
+          to: '#',
+          label: 'Prospect',
+          children: [
+            {
+              to: '#',
+              label: 'Fake Child',
+            },
+          ],
+        },
+        {
+          to: '#',
+          label: 'Alert',
+          children: [
+            {
+              to: '#',
+              label: 'Fake Child',
+            },
+          ],
+        },
+        {
+          to: '#',
+          label: 'Learn',
+          children: [
+            {
+              to: '#',
+              label: 'Fake Child',
+            },
+          ],
+        },
+      ],
+    };
   },
 };
 </script>
