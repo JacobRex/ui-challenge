e2d1b6e8a341f612149fbaf76b84f499dc66bf17
Jacob Rex <jacobrex@gmail.com>
2020-05-07 10:29:53 -0500

    Adding a webpack alias for the components directory

--- nuxt.config.js
+++ nuxt.config.js
@@ -48,6 +48,11 @@ export default {
     ** You can extend webpack config here
     */
     extend (config, ctx) {
+      // Setup Aliases
+      config.resolve.alias = {
+        Components: path.resolve(__dirname, 'components'),
+      };
+
       // Run ESLint on save
       if (ctx.isDev && ctx.isClient) {
         config.module.rules.push({
