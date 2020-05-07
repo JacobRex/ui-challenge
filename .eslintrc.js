9a5b117a4b1a1129cddca1c4f3487f74947851ac
Jacob Rex <jacobrex@gmail.com>
2020-05-07 09:50:17 -0500

    Removing unecessary code

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,13 +11,11 @@ module.exports = {
     'eslint:recommended',
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/recommended',
-    'plugin:jest/recommended'
+    'plugin:vue/recommended'
   ],
   // required to lint *.vue files
   plugins: [
     'vue',
-    'jest'
   ],
   // add your custom rules here
   rules: {
