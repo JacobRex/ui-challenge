be977c1e5191f573c2f6e76143ffdf1c17f8f681
Jacob Rex <jacobrex@gmail.com>
2020-05-07 10:14:56 -0500

    Creating a container component

---
+++ components/Container/src/Container.vue
@@ -0,0 +1,23 @@
+<template>
+  <div :class="$s.Container">
+    <div :class="$s.Content">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UiContainer',
+};
+</script>
+
+<style module="$s">
+@import 'Vars';
+
+.Content {
+  width: calc(100% - (2 * var(--space-xl)));
+  max-width: var(--max-width);
+  margin: auto;
+}
+</style>
