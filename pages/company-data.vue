3c6d9ee6ad293c408420dd0d17642138393876c3
Jacob Rex <jacobrex@gmail.com>
2020-05-07 14:28:20 -0500

    Adding test modal

--- pages/company-data.vue
+++ pages/company-data.vue
@@ -1,11 +1,29 @@
 <template>
   <div>
-    Company Data
+    <ui-button @click="openLayer">
+      Open Layer
+    </ui-button>
   </div>
 </template>
 
 <script>
+import { UiButton } from 'Components/Button';
+
+import NotesModal from './components/NotesModal.vue';
+
 export default {
+  components: {
+    UiButton,
+  },
+
+  inject: ['layerAPI'],
+
+  methods: {
+    openLayer() {
+      this.layerAPI.open(NotesModal);
+    },
+  },
+
   mounted() {
     this.$store.commit(
       'layout/setBreadCrumbs',
